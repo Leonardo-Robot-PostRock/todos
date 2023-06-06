3f4b907620799c235eb8a1516c961d2cf885784f
Leonardo <leonardo38361@gmail.com>
2023-06-06 12:30:10 -0300

    Listar todos

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,25 @@
+import prisma from "@/lib/prisma";
 
-export default function RestTodosPage() {
+export const metadata = {
+    title: 'Listado de Todos',
+    description: 'SEO Title',
+};
+
+export default async function RestTodosPage() {
+    const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
+
+    // useEffect(() => {
+    //     fetch('/api/todos')
+    //         .then(resp => resp.json())
+    //         .then(console.log);
+    // }, [])
+
     return (
         <div>
             <h1>Page RestTodos</h1>
+            {
+                JSON.stringify(todos)
+            }
         </div>
     )
 }
