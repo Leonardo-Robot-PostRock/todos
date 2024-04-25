b208edcfa3491801a7614a209908fde30f06c1c1
Leonardo <leonardo38361@gmail.com>
2024-04-25 17:09:03 -0300

    schema de validación

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma';
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(request: Request) {
 
@@ -24,8 +25,13 @@ export async function GET(request: Request) {
     return NextResponse.json(todos);
 }
 
+const postSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false), 
+})
 
 export async function POST(request: Request) {
+
 
     const body = await request.json();
 
