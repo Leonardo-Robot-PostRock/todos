328cc3072a3e984fff2c45704c421562728c992c
Leonardo <leonardo38361@gmail.com>
2024-04-26 17:01:35 -0300

    creación del post

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -27,15 +27,18 @@ export async function GET(request: Request) {
 
 const postSchema = yup.object({
     description: yup.string().required(),
-    complete: yup.boolean().optional().default(false), 
+    complete: yup.boolean().optional().default(false),
 })
 
 export async function POST(request: Request) {
 
+    try {
+        const { complete, description } = await postSchema.validate(await request.json());
 
-    const body = await request.json();
+        const todo = await prisma.todo.create({ data: { complete, description } })
+        return NextResponse.json(todo);
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 });
+    }
 
-    const todo = await prisma.todo.create({ data: body })
-
-    return NextResponse.json({ body })
 }
