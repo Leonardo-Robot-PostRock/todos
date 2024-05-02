374c7cca6ed3120b1601e199a32849efca57ca5c
Leonardo <leonardo38361@gmail.com>
2024-05-02 10:06:04 -0300

    validaciones en la actualizacion

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma';
+import { Todo } from '@prisma/client';
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 interface Segments {
     params: {
@@ -7,45 +9,50 @@ interface Segments {
     }
 }
 
+const getTodo = async (id: string): Promise<Todo | null> => {
+    const todo = await prisma.todo.findFirst({ where: { id } });
+    return todo;
+}
+
 export async function GET(request: Request, { params }: Segments) {
 
-    const { id } = params;
+    const todo = await getTodo(params.id);
 
-    const todoById = await prisma.todo.findUnique({
-        where: {
-            id
-        }
-    })
-
-    if (!todoById) {
+    if (!todo) {
         return NextResponse.json({ message: 'Todo no encontrado' }, { status: 400 });
     }
 
-    return NextResponse.json(todoById);
+    return NextResponse.json(todo);
 }
+
+const putSchema = yup.object({
+    complete: yup.boolean().optional(),
+    description: yup.string().optional()
+})
 
 export async function PUT(request: Request, { params }: Segments) {
 
     const { id } = params;
 
-    const todoById = await prisma.todo.findUnique({
-        where: {
-            id
-        }
-    })
+    const todo = await getTodo(id);
 
-    if (!todoById) {
+    if (!todo) {
         return NextResponse.json({ message: 'Todo no encontrado' }, { status: 400 });
     }
 
-    const body = await request.json();
+    try {
+
+        const { complete, description } = await putSchema.validate(await request.json());
 
 
-    const updatedTodo = await prisma.todo.update({
-        where: { id },
-        data: { ...body }
-    })
+        const updatedTodo = await prisma.todo.update({
+            where: { id },
+            data: { complete, description }
+        })
 
-    return NextResponse.json(todoById);
+        return NextResponse.json(updatedTodo);
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 });
+    }
 
 }
