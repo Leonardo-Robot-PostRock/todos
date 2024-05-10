be635b10baed063c4728623845aabdc28479ee46
Leonardo <leonardo38361@gmail.com>
2024-05-10 00:49:50 -0300

    Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,26 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
+import { IoCalendarOutline, IoCheckboxOutline, IoListOutline } from 'react-icons/io5';
+
+const menuItem = [
+    {
+        icon: <IoCalendarOutline />,
+        title: 'Dashboard',
+        path: '/dashboard'
+    },
+    {
+        icon: <IoCheckboxOutline />,
+        title: 'Rest TODOS',
+        path: 'dashboard/rest-todos'
+    },
+    {
+        icon: <IoListOutline />,
+        title: 'Server Actions',
+        path: '/dashboard/server-todos'
+    },
+]
 
 export const Sidebar = () => {
     return (
@@ -32,7 +50,11 @@ export const Sidebar = () => {
                 </div>
 
                 <ul className="space-y-2 tracking-wide mt-8">
-                    <SidebarItem />
+                    {
+                        menuItem.map(item => (
+                            <SidebarItem key={item.path} {...item} />
+                        ))
+                    }
                 </ul>
             </div>
 
