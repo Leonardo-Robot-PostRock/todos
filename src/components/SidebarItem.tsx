0114b642feec0531567fa95c8471de974ca4fa09
Leonardo <leonardo38361@gmail.com>
2024-05-10 00:50:08 -0300

    estilos dinamicos

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,14 +1,26 @@
+"use client";
 import Link from 'next/link';
-import { CiBookmarkCheck } from 'react-icons/ci';
+import { usePathname } from 'next/navigation';
 
-export const SidebarItem = () => {
+interface Props {
+    icon: React.ReactNode;
+    path: string;
+    title: string;
+}
 
-    {/* Active className: text-white bg-gradient-to-r from-sky-600 to-cyan-400 */ }
+export const SidebarItem = ({ icon, path, title }: Props) => {
+
+    const pathName = usePathname();
+
     return (
         <li>
-            <Link href="/dashboard/rest-todos" className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400">
-                <CiBookmarkCheck size={30} />
-                <span className="-mr-1 font-medium">Dashboard</span>
+            <Link
+                href={path}
+                className={`px-4 py-3 flex items-center space-x-4 rounded-md group hover:bg-gradient-to-r hover:bg-sky-600 hover:text-white 
+                ${path === pathName ? 'text-white bg-gradient-to-r from-sky-600 to-cyan-400' : ''} 
+            `}>
+                {icon}
+                <span className="group-hover:text-white">{title}</span>
             </Link>
         </li>
 
