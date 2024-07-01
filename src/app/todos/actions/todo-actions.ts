90262808ad75faf99d5627e4a99e5c187fd009cb
Leonardo <leonardo38361@gmail.com>
2024-07-01 09:10:00 -0300

    Creación de promesa

--- src/app/todos/actions/todo-actions.ts
+++ src/app/todos/actions/todo-actions.ts
@@ -3,7 +3,17 @@
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
+
+export const sleep = async (seconds: number = 0) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(true);
+        }, seconds * 1000);
+    })
+}
+
 export const toggleTodo = async (id: string, complete: boolean) => {
+    await sleep(3);
     const todo = await prisma.todo.findFirst({ where: { id } });
 
     if (!todo) {
@@ -25,7 +35,7 @@ export const addTodo = async (description: string) => {
         const todo = await prisma.todo.create({ data: { description } });
         revalidatePath('/dashboard/server-todos');
         return todo;
-        
+
     } catch {
         message: 'Error creando todo'
     }
@@ -37,7 +47,7 @@ export const deleteCompleted = async (): Promise<void> => {
             where: { complete: true }
         })
         revalidatePath('/dashboard/server-todos');
-        
+
     } catch {
         message: 'Error al borrar todo/s'
     }
