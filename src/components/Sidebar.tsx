93592f129174fa93fccf7dff0a22248413295ed2
Leonardo <leonardo38361@gmail.com>
2024-07-07 02:09:13 -0300

    pestaña cookies

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
-import { IoCalendarOutline, IoCheckboxOutline, IoListOutline } from 'react-icons/io5';
+import { IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from 'react-icons/io5';
 
 const menuItem = [
     {
@@ -20,6 +20,11 @@ const menuItem = [
         title: 'Server Actions',
         path: '/dashboard/server-todos'
     },
+    {
+        icon: <IoCodeWorkingOutline />,
+        title: 'Cookies',
+        path: '/dashboard/cookies'
+    }
 ]
 
 export const Sidebar = () => {
