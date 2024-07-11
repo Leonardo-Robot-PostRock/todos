5c437069554b18b6b3d0056ca02dc7325c94475c
Leonardo <leonardo38361@gmail.com>
2024-07-11 20:34:59 -0300

    mejora de función de remover

--- src/shopping-cart/actions/actions.ts
+++ src/shopping-cart/actions/actions.ts
@@ -26,17 +26,23 @@ export const addProductToCart = (id: string) => {
 
 export const removeProductFromCart = (id: string) => {
     const cookieCart = getCookieCart();
-    
+
     delete cookieCart[id];
-    
+
     setCookie('cart', JSON.stringify(cookieCart));
 }
 
 export const removeSingleItemFromCart = (id: string) => {
     const cookieCart = getCookieCart();
 
-    if (cookieCart[id]) {
-        cookieCart[id] -= 1;
+    if (!cookieCart[id]) return;
+
+    const itemsInCart = cookieCart[id] -= 1;
+
+    if (itemsInCart <= 0) {
+        delete cookieCart[id];
+    } else {
+        cookieCart[id] = itemsInCart;
     }
 
     setCookie('cart', JSON.stringify(cookieCart));
