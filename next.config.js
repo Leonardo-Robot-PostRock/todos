07fbfe11944b79ade7f09d4bb702236f0bdcfc14
Leonardo <leonardo38361@gmail.com>
2024-07-16 20:21:20 -0300

    show of attributes improved

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'tailus.io'
+            }, {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com'
             }
         ]
     }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
 import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from 'react-icons/io5';
+import { auth } from '@/auth';
 
 const menuItem = [
     {
@@ -32,7 +33,13 @@ const menuItem = [
     }
 ]
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+    const session = await auth();
+
+    const avatarUrl = session?.user?.image ? session.user.image : '/images/avatar/user.png';
+
+    const userName = session?.user?.name ? session.user.name : 'No name';
+
     return (
         <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
             <div>
@@ -49,13 +56,13 @@ export const Sidebar = () => {
 
                 <div className="mt-8 text-center">
                     <Image
-                        src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+                        src={avatarUrl}
                         alt=""
                         className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
                         width={150}
                         height={150}
                     />
-                    <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+                    <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{userName}</h5>
                     <span className="hidden text-gray-400 lg:block">Admin</span>
                 </div>
 
