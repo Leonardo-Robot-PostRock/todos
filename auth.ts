5beaa4fc7916ea9a99a2ac70b315dce47028f578
Leonardo <leonardo38361@gmail.com>
2024-07-16 02:24:58 -0300

    uso de metodos de NextAuth

--- auth.ts
+++ auth.ts
@@ -1,13 +1,8 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import Github from 'next-auth/providers/github';
 
-export const authOptions: NextAuthConfig = {
-    providers: [
-        Github({
-            clientId: process.env.AUTH_GITHUB_ID!,
-            clientSecret: process.env.AUTH_GITHUB_SECRET!,
-        }),
-    ],
+export const authOptions = {
+    providers: [Github],
 }
 
-export const { handlers } = NextAuth(authOptions);
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
+import { auth, } from "@/auth";
 import { WidgetItem } from "@/components/WidgetItem";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const session = await auth();
+
+    if (!session?.user) {
+        redirect('/api/auth/signin')
+    }
+
     return (
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {/* <WidgetItem /> */}
+        <div className="grid gap-6 md:grid-cols-2 sm:grid-cols-2">
+            <WidgetItem title="Usuario conectado S-Side" >
+                {JSON.stringify(session)}
+            </WidgetItem>
         </div>
     )
 }
