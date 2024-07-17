f09bf63ef27e300548121b8dd02eba8a4cda99c0
Leonardo <leonardo38361@gmail.com>
2024-07-16 23:47:24 -0300

    google provider is added

--- auth.ts
+++ auth.ts
@@ -1,8 +1,9 @@
-import NextAuth, { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
 import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 export const authOptions = {
-    providers: [Github],
+    providers: [Github, Google],
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
