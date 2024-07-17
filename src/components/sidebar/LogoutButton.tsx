ce82a1d681972fea11b2a3627f14fc9a724c5d69
Leonardo <leonardo38361@gmail.com>
2024-07-17 03:48:41 -0300

    button is changed to div

--- src/components/sidebar/LogoutButton.tsx
+++ src/components/sidebar/LogoutButton.tsx
@@ -11,12 +11,11 @@ export const LogoutButton = () => {
 
     if (status === 'loading') {
         return (
-
-            <button
+            <div
                 className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
                 <IoShieldOutline />
                 <span className="group-hover:text-gray-700">Espere...</span>
-            </button>
+            </div>
         )
     }
 
