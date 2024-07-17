81104b86ea44d86dd1fee27fb95d5d28784b8f8d
Leonardo <leonardo38361@gmail.com>
2024-07-17 01:06:11 -0300

    files moved

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { Sidebar } from './Sidebar';
-export { SidebarItem } from './SidebarItem';
+export { Sidebar } from './sidebar/Sidebar';
+export { SidebarItem } from './sidebar/SidebarItem';
+export { LogoutButton } from './sidebar/LogoutButton';
 export { TopMenu } from './TopMenu';
 export { TabBar } from './TabBar'
