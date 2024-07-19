ad5d561c0eee1e95cc8540fd393af82fdcf9c70c
Leonardo <leonardo38361@gmail.com>
2024-07-19 19:25:23 -0300

    delete todo insertion

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -25,12 +25,6 @@ export async function GET(request: Request) {
         }
     })
 
-
-    await prisma.todo.createMany({
-        data: [
-        ]
-    })
-
     return NextResponse.json({
         message: 'Seed executed'
     })
